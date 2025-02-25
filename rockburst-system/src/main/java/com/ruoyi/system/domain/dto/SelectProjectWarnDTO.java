c559e7850e099316dde83ed82f89d6f1a10788d9
shikai <2285058499@qq.com>
2025-02-25 18:49:47 +0800

    修改+

--- rockburst-system/src/main/java/com/ruoyi/system/domain/dto/SelectProjectWarnDTO.java
+++ rockburst-system/src/main/java/com/ruoyi/system/domain/dto/SelectProjectWarnDTO.java
@@ -1,5 +1,6 @@
 package com.ruoyi.system.domain.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -11,10 +12,13 @@ import lombok.Data;
 @Data
 public class SelectProjectWarnDTO {
 
+    @ApiModelProperty(value = "方案名称")
     private String schemeName;
 
+    @ApiModelProperty(value = "计划类型")
     private String planType;
 
+    @ApiModelProperty(value = "状态(0:启用,1:禁用)")
     private String status;
 
 }
--- rockburst-system/src/main/java/com/ruoyi/system/domain/vo/ProjectWarnSchemeVO.java
+++ rockburst-system/src/main/java/com/ruoyi/system/domain/vo/ProjectWarnSchemeVO.java
@@ -20,9 +20,6 @@ public class ProjectWarnSchemeVO extends ProjectWarnSchemeEntity {
     @ApiModelProperty(value = "工作量规则")
     private List<WorkloadRuleDTO> workloadRuleDTOS;
 
-    @ApiModelProperty(value = "距离规则")
-    private DistanceRuleDTO distanceRuleDTO;
-
     @ApiModelProperty(value = "预警类型")
     private String warnType;
 }
