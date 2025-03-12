7d533c2867ee1d65a15e0ee81d0a984dc8998897
shikai <2285058499@qq.com>
2025-03-12 09:33:46 +0800

    修改

--- rockburst-admin/src/main/java/com/ruoyi/web/controller/business/VideoHandleController.java
+++ rockburst-admin/src/main/java/com/ruoyi/web/controller/business/VideoHandleController.java
@@ -2,28 +2,26 @@ package com.ruoyi.web.controller.business;
 
 import com.ruoyi.common.core.domain.R;
 import com.ruoyi.system.service.VideoHandleService;
-import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
 /**
  * @author: shikai
- * @date: 2025/3/7
+ * @date: 2024/11/16
  * @description:
  */
 
-@Api(tags = "视频识别相关")
+@Api(tags = "AI视频识别")
 @RestController
-@RequestMapping("/videoIdentify")
+@RequestMapping(value = "/videoIdentify")
 public class VideoHandleController {
 
     @Resource
     private VideoHandleService videoHandleService;
 
+    @ApiOperation(value = "保存识别后的视频(适用于视频识别)", notes = "保存识别后的视频(适用于视频识别)")
     @GetMapping("/saveUrl")
     public R<Object> saveUrl(@RequestParam String beforeVideoUrl, @RequestParam String afterVideoUrl) {
         return R.ok(videoHandleService.insertT(beforeVideoUrl, afterVideoUrl));
