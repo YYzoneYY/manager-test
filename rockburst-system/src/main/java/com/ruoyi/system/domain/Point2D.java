8004bf4f8f1eefaa93d318cd89e3ffede9b8ee05
dalin <dalin8668@163.com>
2025-05-15 09:41:18 +0800

    wdl  危险区修改

--- rockburst-system/src/main/java/com/ruoyi/system/domain/Point2D.java
+++ rockburst-system/src/main/java/com/ruoyi/system/domain/Point2D.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Setter
 @Getter
@@ -12,6 +13,12 @@ public class Point2D {
     private BigDecimal y;
     private Long areaId;
     private String barType;
+
+    public double distance(Point2D other) {
+        BigDecimal dx = x.subtract(other.x);
+        BigDecimal dy = y.subtract(other.y);
+        return Math.sqrt(dx.multiply(dx).add(dy.multiply(dy)).doubleValue());
+    }
 
     public Point2D() {
     }
@@ -32,5 +39,24 @@ public class Point2D {
         this.y = y;
         this.areaId = areaId;
         this.barType = barType;
+    }
+
+    // equals 和 hashCode 用于 HashMap / HashSet 操作
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Point2D)) return false;
+        Point2D point = (Point2D) o;
+        return x.compareTo(point.x) == 0 && y.compareTo(point.y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x.stripTrailingZeros(), y.stripTrailingZeros());
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x.toPlainString() + "," + y.toPlainString() + ")";
     }
 }
